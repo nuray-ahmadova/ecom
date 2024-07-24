62295a41eae3749de08799095b55b9cfefd5e27c
nurayahmadli111 <nurayahmadova19@gmail.com>
2024-07-25 03:25:43 +0400

    qaldigimyerden devam

---
+++ store/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+#from . import views
+from store.views import *
+
+urlpatterns = [
+    path('', home , name='home'),
+    path('about/', about , name='about'),
+    path('login/', login_user , name='login'),
+    path('logout/', logout_user , name='logout'),
+    path('register/', register_user , name='register'),
+    path('update_password/', update_password , name='update_password'),
+    path('update_user/', update_user , name='update_user'),
+    path('update_info/', update_info , name='update_info'),
+    path('product/<int:pk>', product , name='product'),
+    path('category/<str:foo>', category , name='category'),
+    path('category_summary/<str:foo>', category_summary , name='category_summary'),
+    path('search/', search , name='search'),
+
+
+    
+
+
+]
